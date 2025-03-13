8e4ab8af2ca5da150ea73923f96dc7ee4e234000
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-03-13 16:22:39 +0000

    Removed vite skeleton code | added routing and initial pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+	return (
+		<div className="home">
+			<h1>Welcome to Adamas Kallos</h1>
+			<p>Your destination for luxury beauty treatments.</p>
+		</div>
+	);
+}
+
+export default Home;
---
+++ src/pages/Treatments.jsx
@@ -0,0 +1,10 @@
+function Treatments() {
+	return (
+		<div className="services">
+			<h1>Our Treatments</h1>
+			<p>Explore our range of treatments, from facials to massages.</p>
+		</div>
+	);
+}
+
+export default Treatments;
