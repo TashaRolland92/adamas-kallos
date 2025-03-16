784de4634677f0332684a4bb86a3043a1162da2a
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-03-16 20:37:10 +0000

    tailwind config edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Routes from "./routes/Routes";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
 	return (
 		<>
 			<Routes />
-			<Nav />
+			<Header />
 		</>
 	);
 };
