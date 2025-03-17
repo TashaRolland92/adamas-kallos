70249bc151f63142b4164404bbe2ee75297305d3
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-03-17 15:24:32 +0000

    nav setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Nav from "../components/Nav";
 const Header: React.FC = () => {
 	return (
 		<header className="fixed top-0 left-0 w-full h-14 bg-babyblue">
-			<h1>Logo goes here</h1>
+			<h1 className="playfair-italic-700 text-2xl">Logo goes here</h1>
 			<Nav />
 		</header>
 	);
