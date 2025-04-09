27610c6bdab92d10d5b867723d5b436fa7cd2386
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-04-09 12:09:37 +0100

    used correct API key env var

--- src/services/contentfulClient.ts
+++ src/services/contentfulClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 const client = createClient({
 	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-	accessToken: import.meta.env.VITE_CONTENTFUL_DELIVERY_TOKEN,
+	accessToken: import.meta.env.VITE_CONTENTFUL_API_TOKEN,
 	host: "cdn.contentful.com", // Use "preview.contentful.com" if fetching draft content
 });
 
