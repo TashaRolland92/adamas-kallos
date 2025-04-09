133ce39e06bbe3a2357777b89319835b7a5a2792
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-04-09 16:07:07 +0100

    file added back

---
+++ src/services/contentfulClient.ts
@@ -0,0 +1,9 @@
+import { createClient } from "contentful";
+
+const client = createClient({
+ 	space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
+ 	accessToken: import.meta.env.VITE_CONTENTFUL_API_TOKEN,
+ 	host: "cdn.contentful.com", // Use "preview.contentful.com" if fetching draft content
+});
+
+export default client;
