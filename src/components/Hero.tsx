a9f6948bcb36a49f165084b595cb7cf5f89471d9
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-04-14 09:57:33 +0100

    new book appt component | new tailwind configs

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,17 @@ import { HiMiniArrowTurnRightDown } from "react-icons/hi2";
 
 const Hero: React.FC = () => {
 	return (
-		<section className="hero-section relative h-screen w-full bg-hero-mobile md:bg-hero-desktop retina:md:bg-hero-4k bg-cover bg-center">
-			<div className="hero-content absolute left-2.5 bottom-2.5 text-white m-7">
-				<h1 className="playfair-italic-700 text-6xl sm:text-8xl md:text-9xl">Adamas</h1>
-				<h2 className="playfair-italic-700 text-4xl sm:text-6xl lg:text-8xl mb-2">Kallos</h2>
-				<h3 className="playfair uppercase text-2xl sm:text-4xl lg:6xl">Diamond Beauty</h3>
+		<section className="hero-section h-screen w-full flex items-end bg-hero-mobile md:bg-hero-desktop retina:md:bg-hero-4k bg-cover bg-center">
+			<div className="container mx-auto px-7">
+				<div className="hero-content text-white pt-20 pb-6">
+					<h1 className="playfair-italic-700 text-6xl sm:text-8xl md:text-9xl">Adamas</h1>
+					<h2 className="playfair-italic-700 text-4xl sm:text-6xl lg:text-8xl mb-2">Kallos</h2>
+					<h3 className="playfair uppercase text-2xl sm:text-4xl lg:6xl">Diamond Beauty</h3>
+				</div>
+				<button className="arrow-btn absolute bottom-4 right-2.5">
+					<HiMiniArrowTurnRightDown className="text-white text-8xl" />
+				</button>
 			</div>
-			<button className="arrow-btn absolute bottom-4 right-2.5">
-				<HiMiniArrowTurnRightDown className="text-white text-8xl" />
-			</button>
 		</section>
 	);
 };
