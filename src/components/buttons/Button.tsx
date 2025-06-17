fc81e72b127a24f0d5e50b0af79eb00486ec729b
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-06-17 10:59:42 +0100

    moved button into global component

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+
+type ButtonProps = {
+	to: string; // button "to" required
+	children: React.ReactNode; // button text
+	type?: "button" | "submit" | "reset";
+	onClick?: () => void;
+	disabled?: boolean;
+	classes?: string; // optional styles
+};
+
+const Button = ({
+	to,
+	children,
+	type="button",
+	onClick,
+	disabled = false,
+	classes = ""
+}: ButtonProps) => {
+	const defaultStyles = `
+		${classes}
+		playfair
+		border
+		border-white
+		px-2
+		h-10
+		w-full
+		max-w-36
+		uppercase
+		transition-colors
+		ease-in
+		hover:bg-white
+		hover:text-primaryContent
+	`;
+
+	if(to && !disabled){
+		return(
+			<Link to={to} className={defaultStyles} onClick={onClick}>
+				{children}
+			</Link>
+		);
+	}
+
+	return (
+		<button
+			type={type}
+			onClick={onClick}
+			disabled={disabled}
+			className={defaultStyles}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
