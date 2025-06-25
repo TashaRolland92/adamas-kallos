f0f000dbf484b0cc24bc15981ac8eb32b739ccb5
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-06-25 11:12:48 +0100

    reworked hero for reusability

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { HiMiniArrowTurnRightDown } from "react-icons/hi2";
 
-const Hero: React.FC = () => {
+type HeroProps = {
+	children?: React.ReactNode; // this allows us to insert whatever content we wish and not be confined to the defaults
+	showArrow?: boolean;
+	className?: string;
+	backgroundClass?: string;
+};
+
+const Hero = ({
+	children,
+	showArrow = true,
+	className = '',
+	backgroundClass = '',
+}: HeroProps) => {
+	console.log("Hero background class ", backgroundClass);
 	return (
-		<section className="hero-section h-screen w-full flex items-end bg-hero-mobile md:bg-hero-desktop retina:md:bg-hero-4k bg-cover bg-center">
+		<section className={`hero-section w-full flex items-end bg-cover bg-center ${backgroundClass || 'bg-hero-mobile md:bg-hero-desktop retina:md:bg-hero-4k'} ${className}`}>
 			<div className="container mx-auto px-7">
 				<div className="hero-content text-white pt-20 pb-6">
-					<h1 className="playfair-italic-700 text-6xl sm:text-8xl md:text-9xl">Adamas</h1>
-					<h2 className="playfair-italic-700 text-4xl sm:text-6xl lg:text-8xl mb-2">Kallos</h2>
-					<h3 className="playfair uppercase text-2xl sm:text-4xl lg:6xl">Diamond Beauty</h3>
+					{children}
 				</div>
-				<button className="arrow-btn absolute bottom-4 right-2.5">
-					<HiMiniArrowTurnRightDown className="text-white text-8xl w-16 md:w-24" />
-				</button>
+				{showArrow && (
+					<button className="arrow-btn absolute bottom-4 right-2.5">
+						<HiMiniArrowTurnRightDown className="text-white text-8xl w-16 md:w-24" />
+					</button>
+				)}
 			</div>
 		</section>
 	);
